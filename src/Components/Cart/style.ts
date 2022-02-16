f8a44c07302e423e1f2835cf044fff05b03db032
Gabriela Leal <gabrielamiranda@alu.ufc.br>
2022-02-16 20:44:33 -0300

    style: change margin top cart

--- src/Components/Cart/style.ts
+++ src/Components/Cart/style.ts
@@ -2,7 +2,8 @@ import { makeStyles } from "@material-ui/core";
 
 export const useStyles = makeStyles({
   container: {
-    margin: 0,
+    margin: "20px 0 0 0",
+
     width: "100%",
 
     "@media (min-width: 1000px)": {
